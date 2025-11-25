17f42a62046d6726c36a3f818df1b83c81931879
Pablo Padilla <juanpablopadilla5@gmail.com>
2025-11-25 15:17:46 -0600

    Hotfix, creando un proceso asyncrono para que pueda cargar a tiempo en Render

--- backend/api.py
+++ backend/api.py
@@ -2,13 +2,20 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Form
 from search_engine import build_index_on_startup
 from search_engine import run_real_search
+import asyncio
 
+index_ready = asyncio.Event()
+
+async def start_engine():
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(None, build_index_on_startup)
+    index_ready.set()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("--- Aplication startup ---")
     print("Loading Search Engine")
-    build_index_on_startup()
+    asyncio.create_task(start_engine())
     print("Finished loading search engine")
     yield
     print("Shutting down. . .")
@@ -23,5 +30,10 @@ def root_page():
 
 @app.post('/search/')
 async def search(search_content: str = Form(...)):
+
+    if not index_ready.is_set():
+        return {'error': 'El motor esta inicializandose...'}, 503
+
     print(search_content)
+
     return run_real_search(search_content)
